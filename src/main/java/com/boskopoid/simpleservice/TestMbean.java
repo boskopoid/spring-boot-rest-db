59f03c5a2eb42292a15ea9a87d3d389280752a7d
david <mantelung@gmail.com>
2019-07-17 18:57:00 +0800

    Added log4j in Spring Boot

--- src/main/java/com/boskopoid/simpleservice/TestMbean.java
+++ src/main/java/com/boskopoid/simpleservice/TestMbean.java
@@ -1,14 +1,22 @@
 package com.boskopoid.simpleservice;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @ManagedResource(
         objectName="PD:category=MBeans,name=testBean",
         description="Managed Bean")
 @Component("testMbean")
 public class TestMbean {
+    private static final Logger logger = LogManager.getLogger(TestMbean.class);
+
+    private List<Integer> num = Arrays.asList(1, 2, 3, 4, 5);
 
     private String message = "Simple Message";
 
@@ -24,7 +32,9 @@ public class TestMbean {
 
    @ManagedOperation
     public void setAppParameters(String param, String value){
+       logger.debug("Hello from Log4j 2 - num : {}", () -> num);
         System.out.println("Setting " + param + " to " + value );
+
    }
     public void show(){
         System.out.println(message);
